b1e543c073cd8facc3cce5a0c0ca1d58e4c51932
jocarvajal <42181428+jocarvajal@users.noreply.github.com>
2019-09-20 14:31:14 -0600

    Update simplex.py

--- Proyecto1/venv/include/simplex.py
+++ Proyecto1/venv/include/simplex.py
@@ -5,29 +5,12 @@ def comprobarMax(vector):
             bandera = True
     return bandera
 
-def comprobarMin(vector):
-    bandera = False
-    for valor in vector:
-        if valor > 0:
-            bandera = True
-    return bandera
-
 def encontrarMenor(vector):
     menor = vector[0]
     posicion = 0
     for i in range(len(vector)):
         if vector[i] < menor:
             menor = vector[i]
-            posicion = i
-    return posicion
-
-def encontrarMayor(vector):
-    mayor = vector[0]
-    i = 0
-    posicion = 0
-    for i in range(len(vector)):
-        if vector[i] > mayor:
-            mayor = vector[i]
             posicion = i
     return posicion
 
@@ -56,28 +39,17 @@ def iterarMatriz(matriz,filaPivote,columnaPivote):
                 matriz[i][j] = matriz[i][j] + -multiplo * matriz[filaPivote][j]
     return matriz
 
-
-
-def metodoSimplex(matriz,optimizacion,VB,VNB):
+def metodoSimplex(matriz,VB,VNB):
     ultimaColumna = len(matriz[0]) - 1
-    if optimizacion == "max":
-        while comprobarMax(matriz[0]):
-            columnaPivote = encontrarMenor(matriz[0])
-            filaPivote = coeficienteMenor(matriz, columnaPivote, ultimaColumna)
-            pivote = matriz[filaPivote][columnaPivote]
-            matriz[filaPivote] = prepararFilaPivote(matriz[filaPivote], 1/pivote)
-            matriz = iterarMatriz(matriz,filaPivote,columnaPivote)
-            VB[filaPivote] = VNB[columnaPivote]
+    while comprobarMax(matriz[0]):
+        columnaPivote = encontrarMenor(matriz[0])
+        filaPivote = coeficienteMenor(matriz, columnaPivote, ultimaColumna)
+        pivote = matriz[filaPivote][columnaPivote]
+        matriz[filaPivote] = prepararFilaPivote(matriz[filaPivote], 1/pivote)
+        matriz = iterarMatriz(matriz,filaPivote,columnaPivote)
+        VB[filaPivote] = VNB[columnaPivote]
 
-    else:
-        while comprobarMin(matriz[0]):
-            columnaPivote = encontrarMayor(matriz[0])
-            filaPivote = coeficienteMenor(matriz, columnaPivote, ultimaColumna)
-            pivote = matriz[filaPivote][columnaPivote]
-            matriz[filaPivote] = prepararFilaPivote(matriz[filaPivote], 1/pivote)
-            matriz = iterarMatriz(matriz,filaPivote,columnaPivote)
-    print(VB)
-    print(matriz)
+    
 
 
 matriz = [[-3,-5,0,0,0,0],
@@ -85,7 +57,7 @@ matriz = [[-3,-5,0,0,0,0],
           [0,2,0,1,0,12],
           [3,2,0,0,1,18]]
 
-a = ['U','X3','X4','X5']
-b = ['X1','X2','X3','X4','X5']
+a = ['U','S3','S4','S5']
+b = ['X1','X2','S3','S4','S5']
 
-metodoSimplex(matriz,"max",a,b)
+metodoSimplex(matriz,a,b)
